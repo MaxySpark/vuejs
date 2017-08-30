326743875c8b63c9b01f02610068dc087be3c5cf
MaxySpark <hello@maxyspark.com>
2017-08-31 02:13:13 +0530

    mini project 2 - half

---
+++ Mini-Project-Quote-Manager/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+        
+    </div>
+</template>
+
+<script>
+
+    import QuoteGrid from './components/QuoteGrid.vue';
+
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just A Quote',
+                    'quote 2',
+                    'quote 3',
+                    'quote 4'
+                ],
+                maxQuotes: 10
+            };
+        },
+        components: {
+            appQuoteGrid: QuoteGrid
+        }
+    }
+</script>
+
+<style>
+</style>
